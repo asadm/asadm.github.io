993d0d4f87d0be340a66c18954e0b0ee0e7abaec
Asad Memon <asad.lionpk@gmail.com>
2021-11-06 23:39:07 -0700

    trailingSlash: true

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const withMDX = require('@next/mdx')();
 
 module.exports = withMDX({
   reactStrictMode: true,
+  trailingSlash: true,
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
 });
