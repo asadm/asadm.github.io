53f17526ecce984bfa8ad4bff665c356922e5c4e
Jonathan Solomon <jonatechtv@gmail.com>
2025-06-02 23:06:57 -0600

    Update FloatingResumeButton to link to resume.txt instead of resume.pdf

--- components/FloatingResumeButton.js
+++ components/FloatingResumeButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function FloatingResumeButton() {
   return (
     <div className="floating-resume-button">
-      <a href="https://jonny.sh/resume.pdf" target="_blank" className="project-link">
+      <a href="https://jonny.sh/resume.txt" target="_blank" className="project-link">
         <img src="/assets/img/icons/resume.png" alt="Resume" />
         Get my Latest Resume
       </a>
