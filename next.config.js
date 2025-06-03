33c56d7c67e8a3cd4d83bc25822810ac34c5f75a
Jonathan Solomon <jonatechtv@gmail.com>
2025-06-02 23:17:39 -0600

    Trigger rebuild for eager loading changes

--- next.config.js
+++ next.config.js
@@ -9,3 +9,4 @@ module.exports = withMDX({
 // Force rebuild with correct assets Mon Jun  2 23:09:41 MDT 2025
 // Trigger rebuild after fixing photo.jpg Mon Jun  2 23:12:03 MDT 2025
 // Force clean rebuild Mon Jun  2 23:12:56 MDT 2025
+// Rebuild after adding eager loading to images Mon Jun  2 23:17:39 MDT 2025
