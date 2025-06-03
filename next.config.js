4b9cc8164a619c40f9e90cfcb4f17e4412425f1b
Jonathan Solomon <jonatechtv@gmail.com>
2025-06-02 23:12:56 -0600

    Trigger clean rebuild

--- next.config.js
+++ next.config.js
@@ -8,3 +8,4 @@ module.exports = withMDX({
 // Rebuild trigger Mon Jun  2 23:08:16 MDT 2025
 // Force rebuild with correct assets Mon Jun  2 23:09:41 MDT 2025
 // Trigger rebuild after fixing photo.jpg Mon Jun  2 23:12:03 MDT 2025
+// Force clean rebuild Mon Jun  2 23:12:56 MDT 2025
